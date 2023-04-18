a2dd675c0f335a47c673881283d9f52150b61732
Dipesh Patil <dipeshpatil@Ds-MacBook-Pro.local>
2023-04-18 09:24:18 +0530

    Added logger

--- logger/devLogger.js
+++ logger/devLogger.js
@@ -1,25 +1,31 @@
+const path = require("path");
 const { createLogger, format, transports } = require("winston");
-const { combine, timestamp, label, printf } = format;
+const { combine, timestamp, label, metadata } = format;
 
 const devLogger = () => {
-  const myFormat = printf(({ level, message, timestamp }) => {
-    return `${timestamp} ${level}: ${message}`;
-  });
+  const logFormat = format.printf(
+    (info) => `${info.timestamp} ${info.level} [${info.label}]: ${info.message}`
+  );
 
   return createLogger({
-    level: "debug",
+    level: process.env.NODE_ENV === "production" ? "info" : "debug",
     format: combine(
-      label({ label: "right meow!" }),
-      timestamp({ format: "HH:mm:ss" }),
-      myFormat
+      label({ label: path.basename(process.mainModule.filename) }),
+      timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
+      metadata({
+        fillExcept: ["message", "level", "timestamp", "label"],
+      })
     ),
     transports: [
-      new transports.Console(),
+      new transports.Console({
+        format: format.combine(format.colorize(), logFormat),
+      }),
       new transports.File({
         filename: "./logs/development.log",
-        handleExceptions: true,
+        format: format.combine(logFormat),
       }),
     ],
+    exitOnError: false,
   });
 };
 
--- logger/prodLogger.js
+++ logger/prodLogger.js
@@ -1,34 +1,31 @@
+const path = require("path");
 const { createLogger, format, transports } = require("winston");
-require("winston-mongodb");
-const { combine, timestamp, label, printf } = format;
-
-const { dbConfig } = require("../config/config");
+const { combine, timestamp, label, metadata } = format;
 
 const prodLogger = () => {
-  const myFormat = printf(({ level, message, timestamp }) => {
-    return `${timestamp} ${level}: ${message}`;
-  });
+  const logFormat = format.printf(
+    (info) => `${info.timestamp} ${info.level} [${info.label}]: ${info.message}`
+  );
 
   return createLogger({
-    level: "debug",
+    level: process.env.NODE_ENV === "production" ? "info" : "debug",
     format: combine(
-      label({ label: "right meow!" }),
-      timestamp({ format: "HH:mm:ss" }),
-      myFormat
+      label({ label: path.basename(process.mainModule.filename) }),
+      timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
+      metadata({
+        fillExcept: ["message", "level", "timestamp", "label"],
+      })
     ),
     transports: [
-      new transports.Console(),
+      new transports.Console({
+        format: format.combine(logFormat),
+      }),
       new transports.File({
         filename: "./logs/production.log",
-      }),
-      new transports.MongoDB({
-        db: `mongodb+srv://${dbConfig.username}:${dbConfig.password}@${dbConfig.host}/?retryWrites=true&w=majority`,
-        options: {
-          useNewUrlParser: true,
-          useUnifiedTopology: true,
-        },
+        format: format.combine(logFormat),
       }),
     ],
+    exitOnError: false,
   });
 };
 
