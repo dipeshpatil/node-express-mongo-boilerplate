8b6d83b7c3e72316f5344e4a68fbb4d28e1c01d9
Dipesh Patil <dipeshpatil@Ds-MacBook-Pro.local>
2023-04-18 07:33:36 +0530

    Added Validators and Middlewares

---
+++ validators/user.js
@@ -0,0 +1,13 @@
+const { check } = require("express-validator");
+
+module.exports = {
+  // Checks if name, email and password fields are defined and returns Boolean Array
+  basicUserRegistrationValidator: [
+    check("name", "Name is required").not().isEmpty(),
+    check("email", "Please include a valid email").isEmail(),
+    check(
+      "password",
+      "Please enter a password with 6 or more characters"
+    ).isLength({ min: 6 }),
+  ],
+};
